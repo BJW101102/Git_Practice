2c809aebde9957b7ea77554aefc89ecf80810a81
Brandon Walton <brandonwalton101102@gmail.com>
2024-09-11 15:20:23 -0500

    Reformatted Hello_World.py \n Properly idenent the class to satisfy pip-8 formatting styles.

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,10 @@
 class HelloMessage():
-        def __init__(self, msg: str):
-            self._msg = msg
+    def __init__(self, msg: str):
+        self._msg = msg
 
-        def print_message(self) -> None:
-              """Prints Hello, {user_msg}"""
-              print(f'Hello, {self._msg}')
+    def print_message(self) -> None:
+            """Prints Hello, {user_msg}"""
+            print(f'Hello, {self._msg}')
 
 if __name__ == "__main__":
     h = HelloMessage(msg='World!')
